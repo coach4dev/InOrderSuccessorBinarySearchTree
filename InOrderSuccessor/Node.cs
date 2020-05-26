6fdd758431e3fea10650b40061b75c799efeac67
coach4dev <info@coach4dev.com>
2020-05-26 16:04:16 -0700

    Add project files.

---
+++ InOrderSuccessor/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InOrderSuccessor
+{
+    public class Node
+    {
+        public int value { get; set; }
+        public Node left { get; set; }
+        public Node right { get; set; }
+        public Node parent { get; set; }
+    }
+}
---
+++ InOrderSuccessor/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace InOrderSuccessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Build Tree
+            Node root = new Node() { value = 8 };
+            Node node1 = new Node() { value = 3, parent = root };
+            Node node2 = new Node() { value = 10, parent = root };
+            root.left = node1;
+            root.right = node2;
+
+            Node node3 = new Node() { value = 1, parent = node1 };
+            Node node4 = new Node() { value = 6, parent = node1 };
+            node1.left = node3;
+            node1.right = node4;
+
+            Node node5 = new Node() { value = 4, parent = node4 };
+            Node node6 = new Node() { value = 7, parent = node4 };
+            node4.left = node5;
+            node4.right = node6;
+
+            Node node7 = new Node() { value = 14, parent = node2 };
+            node2.right = node7;
+
+            Node node8 = new Node() { value = 13, parent = node7 };
+            node7.left = node8;
+
+
+            PrintInOrderSuccessor(node1);
+
+            PrintInOrderSuccessor(node5);
+
+            PrintInOrderSuccessor(node6);
+
+            PrintInOrderSuccessor(node7);
+
+            Console.ReadLine();
+        }
+
+        static void PrintInOrderSuccessor(Node n)
+        {
+            var result = GetInOrderSuccessor(n);
+            if (result == null)
+                Console.WriteLine($"The Successor for node with value {n.value} : null");
+            else
+                Console.WriteLine($"The Successor for node with value {n.value} : {result.value}");
+        }
+
+        static Node GetInOrderSuccessor(Node input)
+        {
+            if (input == null)
+                return null;
+            if (input.right != null)
+            {
+                return LeftMostChild(input.right);
+            }
+            else
+            {
+                Node node = input;
+                Node parent = input.parent;
+                if (parent.left == node)
+                    return parent;
+                while (parent != null && parent.left != node)
+                {
+                    node = parent;
+                    parent = parent.parent;
+                }
+                return parent;
+            }
+        }
+
+        static Node LeftMostChild(Node n)
+        {
+            if (n == null)
+                return null;
+            while (n.left != null)
+                n = n.left;
+            return n;
+        }
+    }
+}
